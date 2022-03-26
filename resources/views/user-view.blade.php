48cb5dc50345e5a7eb302f8e0a860199c046f686
FahimSakib <fsakib991@gmail.com>
2022-03-26 13:15:15 +0600

    user details view 2nd part

--- resources/views/user-view.blade.php
+++ resources/views/user-view.blade.php
@@ -1,4 +1,4 @@
-<div class="col md 12">
+<div class="col-md-12">
     <table class="table-borderless">
         <thead>
             <th class="col-md-6">Filed</th>
@@ -14,16 +14,28 @@
             <td>{{ $user->role->role_name }}</td>
         </tr>
         <tr>
-            <td><b>Role</b> </td>
-            <td>{{ $user->role->role_name }}</td>
-        </tr>
-        <tr>
             <td><b>Email</b> </td>
             <td>{{ $user->email }}</td>
         </tr>
         <tr>
             <td><b>Mobile</b> </td>
             <td>{{ $user->mobile_no }}</td>
+        </tr>
+        <tr>
+            <td><b>District</b> </td>
+            <td>{{ $user->district->location_name }}</td>
+        </tr>
+        <tr>
+            <td><b>Upazila</b> </td>
+            <td>{{ $user->upazila->location_name }}</td>
+        </tr>
+        <tr>
+            <td><b>Postal Code</b> </td>
+            <td>{{ $user->postal_code }}</td>
+        </tr>
+        <tr>
+            <td><b>Address</b> </td>
+            <td>{{ $user->address }}</td>
         </tr>
     </tbody>
     </table>
